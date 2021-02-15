677ce214a351e72c952b24c5d5dae4ec228498e2
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-15 23:22:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,7 +22,8 @@ class Song
     MP3Importer.all.collect do |filename|
       split_name = filename.split(" - ")
       song = self.new(split_name[1])
-      artist = Artist.new(split_name[0])
+      artist = Artist.find_or_create_by_name(split_name[0])
+      song.artist = artist
       binding.pry
     end
   end

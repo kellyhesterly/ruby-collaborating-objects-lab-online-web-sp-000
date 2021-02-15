123de4bb331e05fc64129f2abbf32800e0936fcf
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-15 23:27:32 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -19,7 +19,9 @@ class MP3Importer
   end
 
   def import
-    import = MP3Importer.new(path)
+    self.files.each do |file|
+      Song.new_by_filename(file)
+    end
     # new_by_filename = 
   end
 end

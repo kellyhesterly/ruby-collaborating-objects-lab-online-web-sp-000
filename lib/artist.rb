05cec67fd12e2dad1aafaec4bf39bbf69e6c49b0
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-15 18:17:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,7 +7,7 @@ class Artist
 
   def initialize(name)
     @name = name
-    @songs = []
+    # @songs = []
     save
   end
 
@@ -20,7 +20,7 @@ class Artist
   end
 
   def add_song(song)
-    @songs << song
+    # @songs << song
     song.artist = self
   end
 

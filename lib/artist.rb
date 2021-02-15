d528c9e283a175493f1b1e420202d904775930f6
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-15 19:54:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -28,7 +28,7 @@ class Artist
 
   def self.find_or_create_by_name(name)
     artist = Artist.new(name) unless name == self
-    name == self 
+    name == self
     # if name == self
     #   name
     # else

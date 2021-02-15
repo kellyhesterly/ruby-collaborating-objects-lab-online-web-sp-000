34430c2cd16e4534499eb8b55f435cd1228c618a
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-15 23:32:20 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -2,15 +2,8 @@ require 'pry'
 class MP3Importer
   attr_accessor :path
 
-  @@all = []
-
   def initialize(path)
     @path = path
-    save
-  end
-
-  def save
-    @@all << self
   end
 
   def files

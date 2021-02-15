b7db205b64ed068c49c89c93769a5f9fee1ff847
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-15 23:24:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,7 +23,6 @@ class Song
       split_name = filename.split(" - ")
       song = self.new(split_name[1])
       artist = Artist.find_or_create_by_name(split_name[0])
-      song.artist = artist
       binding.pry
     end
   end
